190df03af3e7ada92695fd85c1ff21e92bd8b493
MicaelLeal <mycaellmoura@hotmail.com>
2018-12-17 13:04:41 -0300

    Atualizando adapter main

--- app/src/main/java/com/example/softwareengineering/diminuto/adapters/MainRvAdapter.java
+++ app/src/main/java/com/example/softwareengineering/diminuto/adapters/MainRvAdapter.java
@@ -1,19 +1,29 @@
 package com.example.softwareengineering.diminuto.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.softwareengineering.diminuto.R;
+
+import java.util.List;
+
 
 public class MainRvAdapter extends RecyclerView.Adapter<MainRvAdapter.ViewHolder> {
 
+    private Context context;
+//    private List<>
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
+//        return new ViewHolder(LayoutInflater.from(context)
+//                .inflate(R.layout.todo, parent, false));
     }
 
     @Override
